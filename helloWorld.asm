e81c15092e23f0ea8e7bdcfa6234a32e85dc31d9
Allen <jungal01@luther.edu>
2018-01-22 23:54:22 -0600

    Update helloWorld.asm

--- helloWorld.asm
+++ helloWorld.asm
@@ -1,6 +1,6 @@
-# I have a great interest in assembly language for multiple reasons, one of which is Dr. Knuth's MIX and MMIX computers, and
-# another for simply the nerd factor. This is my attempt to display 'hello world' in asm in Linux. It compiles, but I have no 
-# idea if it's even functional, because it doesn't output to the terminal.
+; I have a great interest in assembly language for multiple reasons, one of which is Dr. Knuth's MIX and MMIX computers, and
+; another for simply the nerd factor. This is my attempt to display 'hello world' in asm in Linux. It compiles, but I have no 
+; idea if it's even functional, because it doesn't output to the terminal.
 
 .text
   .global _start
